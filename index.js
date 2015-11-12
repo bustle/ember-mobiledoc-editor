57bc0bfb3198f91c54effe538b09d69783a8617a
Cory Forsyth <cory.forsyth@gmail.com>
2015-11-12 15:36:15 -0500

    enable as ember-addon

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+
 module.exports = {
-  name: 'ember-mobiledoc-dom-renderer'
+  name: 'ember-mobiledoc-dom-renderer',
+
+  included: function() {
+    var libRoot = require.resolve('mobiledoc-dom-renderer/lib');
+    var libPath = path.dirname(libRoot);
+
+    this.treePaths.addon = libPath;
+  }
 };
