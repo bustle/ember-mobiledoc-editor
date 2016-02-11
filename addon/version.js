56b2899a23d65675aaa288e4e7f6265d376109cd
Cory Forsyth <cory.forsyth@gmail.com>
2016-02-11 17:10:19 -0500

    Expose version

---
+++ addon/version.js
@@ -0,0 +1,8 @@
+import MOBILEDOC_KIT_VERSION from 'mobiledoc-kit/version';
+const EDITOR_VERSION = '0.3.4';
+const VERSION = {
+  EDITOR_VERSION,
+  MOBILEDOC_KIT_VERSION
+};
+
+export default VERSION;
