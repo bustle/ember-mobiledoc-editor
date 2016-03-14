72bc7abad226cc47998c65a9725f15dbec92a5f4
Richard Livsey <richard@livsey.org>
2016-03-14 00:54:53 +0000

    Render atoms, allow renaming via atomNameToComponentName

--- tests/integration/components/render-mobiledoc-test.js
+++ tests/integration/components/render-mobiledoc-test.js
@@ -1,46 +1,65 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-import { CARD_ELEMENT_CLASS } from 'ember-mobiledoc-dom-renderer/components/render-mobiledoc';
+import { CARD_ELEMENT_CLASS, ATOM_ELEMENT_CLASS } from 'ember-mobiledoc-dom-renderer/components/render-mobiledoc';
 
 moduleForComponent('render-mobiledoc', 'Integration | Component | render-mobiledoc', {
   integration: true
 });
 
 let mobiledoc = {
-  version: '0.2.0',
+  version: '0.3.0',
+  markups: [],
+  atoms: [],
+  cards: [],
   sections: [
-    [],
-    [
-      [1, 'P', [
-        [[], 0, 'Hello, world!']
-      ]]
-    ]
+    [1, 'P', [
+      [0, [], 0, 'Hello, world!']
+    ]]
   ]
 };
 
 let mobiledoc2 = {
-  version: '0.2.0',
+  version: '0.3.0',
+  markups: [],
+  atoms: [],
+  cards: [],
   sections: [
-    [],
-    [
-      [1, 'P', [
-        [[], 0, 'Goodbye, world!']
-      ]]
-    ]
+    [1, 'P', [
+      [0, [], 0, 'Goodbye, world!']
+    ]]
   ]
 };
 
 let cardName = 'sample-test-card';
 
 let mobiledocWithCard = {
-  version: '0.2.0',
+  version: '0.3.0',
+  markups: [],
+  atoms: [],
+  cards: [
+    [cardName, {foo: 'bar'}]
+  ],
   sections: [
-    [],
-    [
-      [10, cardName, {foo: 'bar'}]
-    ]
+    [10, 0]
   ]
 };
+
+let atomName = 'sample-test-atom';
+
+let mobiledocWithAtom = {
+  version: '0.3.0',
+  markups: [],
+  atoms: [
+    [atomName, 'value', {foo: 'bar'}]
+  ],
+  cards: [],
+  sections: [
+    [1, 'P', [
+      [1, [], 0, 0]
+    ]]
+  ]
+};
+
 
 test('it renders mobiledoc', function(assert) {
   this.set('mobiledoc', mobiledoc);
@@ -84,6 +103,47 @@ test('it uses `cardNameToComponentName` to allow selecting components', function
             'renders card payload');
   assert.equal(passedCardName, 'sample-test-card',
                'calls `cardNameToComponentName` with correct card');
+});
+
+test('it renders mobiledoc with atoms', function(assert) {
+  this.set('mobiledoc', mobiledocWithAtom);
+  this.set('atomNames', [atomName]);
+  this.render(hbs`{{render-mobiledoc mobiledoc=mobiledoc atomNames=atomNames}}`);
+
+  assert.ok(this.$('#sample-test-atom').length, 'renders atom template');
+  assert.ok(this.$('#sample-test-atom:contains(value: value)').length,
+            'renders atom value');
+  assert.ok(this.$('#sample-test-atom:contains(payload: bar)').length,
+            'renders atom payload');
+  assert.ok(this.$('.' + ATOM_ELEMENT_CLASS).length,
+            `renders atom with class ${ATOM_ELEMENT_CLASS}`);
+  assert.ok(this.$('.' + ATOM_ELEMENT_CLASS + '-' + atomName).length,
+            `renders atom with class ${ATOM_ELEMENT_CLASS}-${atomName}`);
+});
+
+test('it uses `atomNameToComponentName` to allow selecting components', function(assert) {
+  this.set('mobiledoc', mobiledocWithAtom);
+  this.set('atomNames', [atomName]);
+
+  let passedAtomName;
+  let Component = this.container.lookupFactory('component:render-mobiledoc');
+  this.registry.register('component:my-render-mobiledoc', Component.extend({
+    atomNameToComponentName(atomName) {
+      passedAtomName = atomName;
+      return 'sample-changed-name-test-atom';
+    }
+  }));
+
+  this.render(hbs`{{my-render-mobiledoc mobiledoc=mobiledoc atomNames=atomNames}}`);
+
+  assert.ok(this.$('#sample-changed-name-test-atom').length,
+            'renders atom template');
+  assert.ok(this.$('#sample-changed-name-test-atom:contains(value: value)').length,
+            'renders atom value');
+  assert.ok(this.$('#sample-changed-name-test-atom:contains(payload: bar)').length,
+            'renders atom payload');
+  assert.equal(passedAtomName, 'sample-test-atom',
+               'calls `atomNameToComponentName` with correct atom');
 });
 
 
