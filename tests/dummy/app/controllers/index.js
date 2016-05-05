fa8e5ea047d9a459e7692a21a933f0cdccd29ddc
Cory Forsyth <cory.forsyth@gmail.com>
2016-05-05 16:29:31 -0400

    Use `willRender` instead of `didInsertElement`, make fastboot-compatible
    Also update the dummy app and add other refactorings to simplify
    the renderer component.

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,24 +1,54 @@
 import Ember from 'ember';
 
-let { computed } = Ember;
+let mobiledocs = {
+  simple: {
+    version: '0.3.0',
+    markups: [],
+    cards: [],
+    atoms: [],
+    sections: [
+      [1, 'P', [
+        [0, [], 0, 'Hello world!']
+      ]]
+    ]
+  },
+  card: {
+    version: '0.3.0',
+    markups: [],
+    cards: [['sample-card', {}]],
+    atoms: [],
+    sections: [
+      [10, 0]
+    ]
+  },
+  atom: {
+    version: '0.3.0',
+    markups: [],
+    cards: [],
+    atoms: [['sample-test-atom', 'bob', {foo: 'bar'}]],
+    sections: [
+      [1, 'P', [
+        [0, [], 0, 'Hello card'],
+        [1, 0, [], 0],
+        [0, [], 0, '!']
+      ]]
+    ]
+  }
+};
 
 export default Ember.Controller.extend({
+  init() {
+    this._super(...arguments);
+    this.set('mobiledoc', mobiledocs['simple']);
+    this.set('mobiledocNames', Object.keys(mobiledocs));
+  },
+
   cardNames: ['sample-card'],
-  mobiledoc: computed(function() {
-    return {
-      version: '0.2.0',
-      sections: [
-        [],
-        [
-          [1, 'P', [
-            [[], 0, 'Hello world!']
-          ]],
-          [10, 'sample-card', {foo: 'bar'}],
-          [1, 'P', [
-            [[], 0, 'Hello world!']
-          ]],
-        ]
-      ]
-    };
-  })
+  atomNames: ['sample-test-atom'],
+
+  actions: {
+    selectMobiledoc({target: {value}}) {
+      this.set('mobiledoc', mobiledocs[value]);
+    }
+  }
 });
