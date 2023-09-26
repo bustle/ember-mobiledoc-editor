c76031c61fa3ddf21d5e651890e8e1b02978cb46
Cyril Fluck <github@fluck.fr>
2023-09-26 12:57:57 -0700

    [BUG] Import mobiledoc-kit css

--- index.js
+++ index.js
@@ -3,4 +3,10 @@
 
 module.exports = {
   name: require('./package').name,
+
+  included: function (app) {
+    this._super.included.apply(this, app);
+
+    app.import('node_modules/mobiledoc-kit/dist/mobiledoc.css');
+  },
 };
